e9df65f553480593c8288ec52b45ea75111a9ef3
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-03-21 14:34:25 -0700

    last minute changes

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -5,6 +5,7 @@ import SearchFlightForm from '../../components/SearchFlightForm/SearchFlightForm
 import SearchRouteForm from '../../components/SearchRouteForm/SearchRouteForm';
 import Status from '../Status/Status';
 import { useNavigate } from 'react-router-dom';
+import globe from '../../assets/earth.png';
 
 export default function Search() {
     const [searchMethod, setSearchMethod] = useState('number');
@@ -32,12 +33,19 @@ export default function Search() {
             <main>
                 <section className='search'>
                     <h1 className='search__title'>Search Flight</h1>
-                    <div className='search__method'>
-                        <button className={`search__method-number ${searchMethod === 'number' ? 'search__method--selected' : ''}`} onClick={handleSearchMethod}>By Flight Number</button>
-                        <button className={`search__method-route ${searchMethod === 'route' ? 'search__method--selected' : ''}`} onClick={handleSearchMethod}>By Route</button>
-                    </div>
-                    <div className='search__input'>
-                        {searchMethod === 'number' ? <SearchFlightForm formHandler={formHandler} /> : <SearchRouteForm formHandler={formHandler} />}
+                    <div className='search__container'>
+                        <div className='search__container__left'>
+                            <div className='search__method'>
+                                <button className={`search__method-number ${searchMethod === 'number' ? 'search__method--selected' : ''}`} onClick={handleSearchMethod}>By Flight Number</button>
+                                <button className={`search__method-route ${searchMethod === 'route' ? 'search__method--selected' : ''}`} onClick={handleSearchMethod}>By Route</button>
+                            </div>
+                            <div className='search__input'>
+                                {searchMethod === 'number' ? <SearchFlightForm formHandler={formHandler} /> : <SearchRouteForm formHandler={formHandler} />}
+                            </div>
+                        </div>
+                        <div className='search__img-container'>
+                            <img className='search__img' src={globe} alt='globe' />
+                        </div>
                     </div>
                 </section>
             </main>
