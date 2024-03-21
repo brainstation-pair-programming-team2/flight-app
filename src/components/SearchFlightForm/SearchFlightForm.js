2fd0d145f8829fe777f278c31d31a263c9ab307c
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-03-21 11:05:06 -0700

    add form field validation

--- src/components/SearchFlightForm/SearchFlightForm.js
+++ src/components/SearchFlightForm/SearchFlightForm.js
@@ -1,9 +1,41 @@
 import './SearchFlightForm.scss'
 
 export default function SearchFlightForm() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const inputs = form.querySelectorAll('input');
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() - 1);
+
+        let isValid = true;
+
+        inputs.forEach((input) => {
+            if (input.value.trim() === '') {
+                input.classList.add('invalid');
+                isValid = false;
+            } else {
+                input.classList.remove('invalid');
+            }
+        });
+
+        const selectedDate = new Date(inputs[1].value);
+        if (inputs[1].value.trim() === '' || selectedDate <= currentDate) { 
+            inputs[1].classList.add('invalid');
+            isValid = false;
+        } else {
+            inputs[1].classList.remove('invalid');
+        }
+
+        if (isValid) {
+            
+        }
+    };
+
     return (
         <>
-            <form className='flight'>
+            <form className='flight' onSubmit={handleSubmit}>
                 <label className='flight__number'>
                     Flight Number
                     <input type="text" name="flight-number" id="flight-number" placeholder='Enter Flight Number' />
--- src/components/SearchRouteForm/SearchRouteForm.js
+++ src/components/SearchRouteForm/SearchRouteForm.js
@@ -1,23 +1,60 @@
 import './SearchRouteForm.scss'
 
 export default function SearchRouteForm() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const inputs = form.querySelectorAll('input');
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() - 1);
+
+        let isValid = true;
+
+        inputs.forEach((input) => {
+            if (input.value.trim() === '') {
+                input.classList.add('invalid');
+                isValid = false;
+            } else {
+                input.classList.remove('invalid');
+            }
+        });
+
+        const selectedDate = new Date(inputs[3].value);
+        if (inputs[3].value.trim() === '' || selectedDate <= currentDate) { 
+            inputs[3].classList.add('invalid');
+            isValid = false;
+        } else {
+            inputs[3].classList.remove('invalid');
+        }
+
+        if (isValid) {
+            
+        }
+    };
+
+
     return (
         <>
-            <form className='route'>
+            <form className='route' onSubmit={handleSubmit}>
                 <label className='route__from'>
+                    Departure From
                     <input type="text" name="flight-from" id="flight-from" placeholder='Enter Departure City' />
                 </label>
                 <label className='route__to'>
+                    Arrival To
                     <input type="text" name="flight-to" id="flight-to" placeholder='Enter Arrival City' />
                 </label>
-                <label className='route__arline'>
+                <label className='route__airline'>
+                    Airline
                     <input type="text" name="flight-airline" id="flight-airline" placeholder='Enter Airline' />
                 </label>
                 <label className='route__date'>
+                    Date
                     <input type="date" name="flight-date" id="flight-date" />
                 </label>
                 <label className='route__submit'>
-                    <button type='submit'>Search</button>
+                    <button type='submit' className='route__submit-button'>Search</button>
                 </label>
             </form>
         </>
